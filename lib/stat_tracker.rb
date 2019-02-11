0fa286679b749c6e03f5fdd86235363da7bc93a1
Carrie Walsh <carriewalsh@gmail.com>
2019-02-11 15:51:38 -0700

    Finishes StatTracker.from_csv - no specifics

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,13 @@
 
 class StatTracker
+  def initialize
+  end
+
+
+  def self.from_csv(locations)
+    locations.each do |location|
+      # puts location.last
+    end
+    StatTracker.new
+  end
 end
