d8730609e92b520192778ba0a02d27a07a5970a1
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-12 14:39:05 -0700

    Adds GameTeamRepo instantiation

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,11 @@
 
 class StatTracker
-  attr_accessor :games
+  attr_accessor :games, :game_teams
 
   def self.from_csv(locations)
     st = StatTracker.new
     st.games = GameRepo.new(locations[:games],self)
+    st.game_teams = GameTeamRepo.new(locations[:game_teams],self)
     st
   end
 
