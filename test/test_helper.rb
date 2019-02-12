734b400ed3f80d0447280abaa2ab279922b331ec
Chi Tran <chidtran@gmail.com>
2019-02-12 10:19:17 -0700

    Adds requires to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,4 +8,7 @@ require "pry"
 require "./lib/stat_tracker"
 require "./lib/game_team"
 require "./lib/game"
-
+require "./lib/team"
+require "./lib/season_stats"
+require "./lib/team_stats"
+require "./lib/league_stats"
