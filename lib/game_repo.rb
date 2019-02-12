7152ad04deec944d579658fad2849e6af58f7c90
Carrie Walsh <carriewalsh@gmail.com>
2019-02-12 11:57:12 -0700

    Changes StatTracker to auto create GameRepo

--- lib/game_repo.rb
+++ lib/game_repo.rb
@@ -1,10 +1,11 @@
 class GameRepo < StatTracker
-  attr_reader :repo
-  def initialize
+  attr_reader :repo, :parent
+  def initialize(filename, parent)
     @repo = []
-    contents = CSV.open './data/game_fixture.csv', headers: true, header_converters: :symbol
+    contents = CSV.open filename, headers: true, header_converters: :symbol
     contents.each do |row|
       @repo << Game.new(row)
     end
+    @parent = parent
   end
 end
