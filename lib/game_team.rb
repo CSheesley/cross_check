07b9a79e63241c2a210bd3a5c1236209a0d1ee5f
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-12 16:05:34 -0700

    Renames some keys in initialize method to match csv

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -17,19 +17,19 @@ class GameTeam
   def initialize(info, parent)
     @game_id = info[:game_id].to_i
     @team_id = info[:team_id].to_i
-    @home_or_away = info[:home_or_away]
+    @home_or_away = info[:hoa]
     @won = info[:won]
     @settled_in = info[:settled_in]
     @head_coach = info[:head_coach]
-    @goals = info[:goals]
-    @shots = info[:shots]
-    @hits = info[:hits]
-    @pim = info[:pim]
-    @power_play_chances = info[:power_play_chances]
-    @power_play_goals = info[:power_play_goals]
-    @face_off_win_percentage = info[:face_off_win_percentage]
-    @giveaways = info[:giveaways]
-    @takeaways = info[:takeaways]
+    @goals = info[:goals].to_i
+    @shots = info[:shots].to_i
+    @hits = info[:hits].to_i
+    @pim = info[:pim].to_i
+    @power_play_chances = info[:powerplayopportunities].to_i
+    @power_play_goals = info[:power_play_goals].to_i
+    @face_off_win_percentage = info[:faceoffwinpercentage].to_f.round(2)
+    @giveaways = info[:giveaways].to_i
+    @takeaways = info[:takeaways].to_i
 
     @parent = parent
   end
