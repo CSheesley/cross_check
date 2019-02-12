5da5fc569da08b97f124a14a30b105b8d9cb97ff
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-12 11:20:15 -0700

    Adds expected result classes for clarification

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -2,36 +2,41 @@ require "./test/test_helper"
 
 class SeasonStatsTest < Minitest::Test
 
-  def setup
+  # def setup
   #   @season_stats = SeasonStats.new(season_info)
   # end
   #
   # def test_biggest_bust_and_suprise_of_season_is_shown
   #   skip
+  #   expected = String
   #   assert_equal expected, @season_stats.biggest_bust(season_id)
   #   assert_equal expected, @season_stats.biggest_suprise(season_id)
   # end
   #
   # def test_best_and_worst_coach_by_win_percentage_for_season_are_shown
   #   skip
+  #   expected = String
   #   assert_equal expected, @season_stats.winningest_coach(season_id)
   #   assert_equal expected, @season_stats.worst_coach(season_id)
   # end
   #
   # def test_most_and_least_accurate_teams_of_season_are_shown
   #   skip
+  #   expected = String
   #   assert_equal expected, @season_stats.most_accurate_team(season_id)
   #   assert_equal expected, @season_stats.least_accurate_team((season_id)
   # end
   #
   # def test_most_and_least_hitting_teams_of_season_are_shown
   #   skip
+  #   expected = String
   #   assert_equal expected, @season_stats.most_hits(season_id)
   #   assert_equal expected, @season_stats.least_hits(season_id)
   # end
   #
   # def test_percentage_of_goals_which_were_power_play_goals_for_season_are_shown
   #   skip
+  #   expected = Float (nearest_100th)
   #   assert_equal expected, @season_stats.power_play_goal_percentage(season_id)
   # end
 end
