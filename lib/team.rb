aa8d23e2b441d9d898cae9ce4035b894e7315a14
Chi Tran <chidtran@gmail.com>
2019-02-11 20:33:33 -0700

    Change team_id and franchise_id to strings by defaults then to_i

--- lib/team.rb
+++ lib/team.rb
@@ -2,8 +2,8 @@ class Team
   attr_reader :team_id, :franchise_id, :short_name, :team_name, :abbreviation 
 
   def initialize(team_info)
-    @team_id      = team_info[:team_id]
-    @franchise_id = team_info[:franchise_id] #on the csv, it is #franchiseID
+    @team_id      = team_info[:team_id].to_i
+    @franchise_id = team_info[:franchise_id].to_i #on the csv, it is #franchiseID
     @short_name   = team_info[:short_name] #on the csv, it is shortName -- should we change short_name to something else or is that really what it's supposed ot be called? 
     @team_name    = team_info[:team_name] #csv, teamName - I don't think these should matter, right? 
     @abbreviation = team_info[:abbreviation] 
