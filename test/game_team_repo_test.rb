bc4874bc2e3295f7daae5877add709923301c721
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-13 14:49:31 -0700

    Changes setup in test to reference @game_team_repo instead of @st

--- test/game_team_repo_test.rb
+++ test/game_team_repo_test.rb
@@ -1,26 +1,19 @@
 require "./test/test_helper"
-require "csv"
 
 class GameTeamRepoTest < MiniTest::Test
   def setup
-    @game_path = './data/game_fixture.csv'
-    @team_path = './data/team_info.csv'
-    @game_teams_path = './data/game_team_stats_fixture.csv'
-    @locations = {games: @game_path,
-                  teams: @team_path,
-                  game_teams: @game_teams_path}
-    @st = StatTracker.from_csv(@locations)
+    @game_team_repo = GameTeamRepo.new('./data/game_team_stats_fixture.csv', "StatTracker")
   end
 
   def test_game_team_repo_exists
-    assert_instance_of GameTeamRepo, @st.game_teams
+    assert_instance_of GameTeamRepo, @game_team_repo
   end
 
   def test_game_team_repo_has_game_teams
-    assert_equal 30, @st.game_teams.repo.count
+    assert_equal 30, @game_team_repo.repo.count
   end
 
   def test_it_knows_who_its_parent_is
-    assert_equal StatTracker, @st.game_teams.parent
+    assert_equal "StatTracker", @game_team_repo.parent
   end
 end
