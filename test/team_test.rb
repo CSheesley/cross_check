3c9f04e0fcaa88a054abd532ba79646f625055d8
Chi Tran <chidtran@gmail.com>
2019-02-13 14:26:17 -0700

    Merges Team test

--- test/team_test.rb
+++ test/team_test.rb
@@ -2,15 +2,14 @@ require "./test/test_helper"
 
 class TeamTest < Minitest::Test
   def setup
-
     team_info = {team_id: "6", 
       franchise_id: "6", 
       short_name: "Boston", 
       team_name: "Bruins", 
       abbreviation: "BOS",
       link: "/api/v1/teams/6"}
+
     @team = Team.new(team_info)
-  
     # @game_path = './data/game_fixture.csv'
     # @team_path = './data/team_info.csv'
     # @game_teams_path = './data/game_team_stats_fixture.csv'
@@ -18,7 +17,6 @@ class TeamTest < Minitest::Test
     #               teams: @team_path,
     #               game_teams: @game_teams_path}
     # @st = StatTracker.from_csv(@locations)
-
   end
 
   def test_team_exists
