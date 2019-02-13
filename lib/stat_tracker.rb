3fb0703b77170d8ebde0e648a62c88b4effe1a38
Chi Tran <chidtran@gmail.com>
2019-02-12 18:04:14 -0700

    Adds teams/TeamRepo to init method in StatTracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,13 +3,11 @@ class StatTracker
   attr_accessor :games, :game_teams
 
   def self.from_csv(locations)
-    st = StatTracker.new
-    st.games = GameRepo.new(locations[:games],self)
-    st.game_teams = GameTeamRepo.new(locations[:game_teams],self)
-    st
+    st            = StatTracker.new
+    st.games      = GameRepo.new(locations[:games], self)
+    st.game_teams = GameTeamRepo.new(locations[:game_teams], self)
+    st.teams      = TeamRepo.new(locations[:teams], self)
   end
-
-
 end
 
 
