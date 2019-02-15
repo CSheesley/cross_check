a66bb8520a5f885b18f5af9321b7d79888e5f9ad
Carrie Walsh <carriewalsh@gmail.com>
2019-02-15 12:40:06 -0700

    Adds helper method most_and_least

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -27,30 +27,112 @@ module SeasonStats
 
 
 
-  def most_accurate_team
+  def most_accurate_team(season)
+    find_most_or_least_for_season(season, "accuracy", "most")
     # min_by ratio of shots on goal to goals shots:goals
     #string of team name
   end
 
 
-  def least_accurate_team
+  def least_accurate_team(season)
+    find_most_or_least_for_season(season, "accuracy", "least")
     # max_by ratio of shots on goal to goals shots:goals
     #string of team name
   end
 
 
-  def most_hits
+  def most_hits(season)
+    find_most_or_least_for_season(season, "hits", "most")
     #team with most hits
     #IN GIVEN SEASON
     #string of team name
   end
 
 
-  def least_hits
+  def find_games_by_season(season)
+    @game_teams.repo.find_all do |game_team|
+      game_id_to_season(game_team.game_id) == season
+    end
+  end
+
+  def find_most_or_least_for_season(season, attribute, most_or_least)
+    list = find_games_by_season(season)
+    team = nil
+    most = 0
+    sum = 0
+    least = 5000 #what should I put here
+    if most_or_least == "most"
+      if attribute == "hits"
+        list.each do |game_team|
+          games = get_all_game_teams_for_team(game_team.team_id)
+          sum = games.sum do |game|
+            game.hits
+          end
+          if sum > most
+            most = sum
+            team = game_team.team_id
+          end
+        end
+        team_id_swap(team)
+      elsif attribute == "accuracy"
+        list.each do |game_team|
+          games = get_all_game_teams_for_team(game_team.team_id)
+          shots = games.sum do |game|
+            game.shots
+          end
+          goals = games.sum do |game|
+            game.goals
+          end
+          ratio = shots / goals.to_f
+          if ratio > most
+            least = ratio
+            team = game_team.team_id
+          end
+        end
+        team_id_swap(team)
+      end
+    else
+      if attribute = "hits"
+        list.each do |game_team|
+          games = get_all_game_teams_for_team(game_team.team_id)
+          sum = games.sum do |game|
+            game.hits
+          end
+          if sum < least
+            least = sum
+            team = game_team.team_id
+          end
+        end
+        team_id_swap(team)
+      elsif attribute == "accuracy"
+        list.each do |game_team|
+          games = get_all_game_teams_for_team(game_team.team_id)
+          shots = games.sum do |game|
+            game.shots
+          end
+          goals = games.sum do |game|
+            game.goals
+          end
+          ratio = shots / goals.to_f
+          if ratio < least
+            least = ratio
+            team = game_team.team_id
+          end
+        end
+        team_id_swap(team)
+      end
+    end
+  end
+
+
+  def least_hits(season)
+    find_most_or_least_for_season(season,"hits","least")
     #team with fewest hits
     #IN GIVEN SEASON
     #string of team name
   end
+
+
 
   def game_id_to_season(game_id)
     game_team = @games.repo.find do |game|
@@ -59,10 +141,10 @@ module SeasonStats
     game_team.season
   end
 
+
+
   def power_play_goal_percentage(season)
-    list = @game_teams.repo.find_all do |game_team|
-      game_id_to_season(game_team.game_id) == season
-    end
+    list = find_games_by_season(season)
     pp_goals = list.reduce(0) do |sum, game_team|
       sum += game_team.power_play_goals
     end
--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -3,9 +3,9 @@ require "./test/test_helper"
 class SeasonStatsTest < Minitest::Test
 
   def setup
-    @game_path = './data/game_fixture.csv'
+    @game_path = "./data/game_stats_w_reg.csv"
     @team_path = './data/team_info.csv'
-    @game_teams_path = './data/game_team_stats_fixture.csv'
+    @game_teams_path = './data/game_stats_same_as_games.csv'
     @locations = {games: @game_path,
                   teams: @team_path,
                   game_teams: @game_teams_path}
@@ -14,15 +14,8 @@ class SeasonStatsTest < Minitest::Test
 
   def test_biggest_bust_and_suprise_of_season_is_shown
     skip
-    game_path = "./data/game_stats_w_reg.csv"
-    team_path = './data/team_info.csv'
-    game_teams_path = './data/game_team_stats_fixture.csv'
-    locations = {games: game_path,
-                  teams: team_path,
-                  game_teams: game_teams_path}
-    st2 = StatTracker.from_csv(locations)
-    assert_equal "Rangers", st2.biggest_bust(20122013)
-    assert_equal "Islanders", st2.biggest_suprise(20122013)
+    assert_equal "Rangers", @st.biggest_bust(20122013)
+    assert_equal "Islanders", @st.biggest_suprise(20122013)
   end
   #
   def test_best_and_worst_coach_by_win_percentage_for_season_are_shown
@@ -33,30 +26,34 @@ class SeasonStatsTest < Minitest::Test
   #
   def test_most_and_least_accurate_teams_of_season_are_shown
     skip
-    assert_equal "Red Wings", @st.most_accurate_team(20122013)
-    assert_equal "Penguins", @st.least_accurate_team(20122013)
+    assert_equal "Rangers", @st.most_accurate_team(20122013)
+    assert_equal "Bruins", @st.least_accurate_team(20122013)
   end
-  #
+
+
+  def test_can_find_most_in_season
+    assert_equal "Rangers", @st.find_most_or_least_for_season(20122013, "hits", "most")
+    assert_equal "Rangers", @st.find_most_or_least_for_season(20122013, "accuracy", "most")
+  end
+
+  def test_can_find_least_in_season
+    assert_equal "Bruins", @st.find_most_or_least_for_season(20122013,"hits","least")
+    assert_equal "Bruins", @st.find_most_or_least_for_season(20122013,"accuracy", "least")
+  end
+
+
   def test_most_and_least_hitting_teams_of_season_are_shown
-    skip
-    assert_equal "Bruins", @st.most_hits(20122013)
-    assert_equal "Blackhawks", @st.least_hits(20122013)
+
+    assert_equal "Rangers", @st.most_hits(20122013)
+    assert_equal "Bruins", @st.least_hits(20122013)
   end
-  #
+
 
   def test_can_convert_game_id_to_season
-    assert_equal 20122013, @st.game_id_to_season(2012030111)
+    assert_equal 20122013, @st.game_id_to_season(2012030221)
   end
 
   def test_percentage_of_power_play_goals_for_season
-    game_path = "./data/game_stats_w_reg.csv"
-    team_path = './data/team_info.csv'
-    game_teams_path = './data/game_stats_same_as_games.csv'
-    locations = {games: game_path,
-                  teams: team_path,
-                  game_teams: game_teams_path}
-    st2 = StatTracker.from_csv(locations)
-
-    assert_equal 23.08, st2.power_play_goal_percentage(20122013)
+    assert_equal 23.08, @st.power_play_goal_percentage(20122013)
   end
 end
