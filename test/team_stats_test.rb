41a54c74c0d9f3c49fc240ee157108ef412ed6a1
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-17 12:03:40 -0700

    Adds passing test functionality for team_info, rival, and favorite_opponent methods

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -2,14 +2,13 @@ require "./test/test_helper"
 
 class TeamStatsTest < Minitest::Test
 
-  def setup #required different setup, using custom CSV fixture
+  def setup
     @game_path = './data/game_fixture.csv'
     @team_path = './data/team_info.csv'
     @game_teams_path = './data/game_team_stats_fixture_corey.csv'
     @locations = {games: @game_path,
       teams: @team_path,
       game_teams: @game_teams_path}
-
     @stat_tracker = StatTracker.from_csv(@locations)
   end
 
@@ -17,22 +16,19 @@ class TeamStatsTest < Minitest::Test
     assert_instance_of StatTracker, @stat_tracker
   end
 
-  # not working as expected... may need to remove @parent
-  # example below. Need all but @parent attribute
-  # @abbreviation="NJD",
-  # @franchise_id=23,
-  # @link="/api/v1/teams/1",
-  # @parent=#<TeamRepo:0x007f89b6968808 ...>,
-  # @short_name="New Jersey",
-  # @team_id=1,
-  # @team_name="Devils">,
-  # def test_team_info_with_all_attributes_is_shown
-  #   #expected = Hash
-  #   assert_instance_of Hash, @stat_tracker.team_info(17)
-  # end
+  def test_team_info_can_be_shown_in_a_hash_of_six_attributes
+    expected = {
+      "team_id" => 17,
+      "franchise_id" => 12,
+      "short_name" => "Detroit",
+      "team_name" => "Red Wings",
+      "abbreviation" => "DET",
+      "link" => "/api/v1/teams/17"
+    }
+    assert_equal expected, @stat_tracker.team_info(17)
+  end
 
-  def test_all_games_played_can_be_gathered #helper
-    # binding.pry
+  def test_all_games_played_can_be_gathered
     assert_equal 6, @stat_tracker.all_games_played(17).count
   end
 
@@ -51,36 +47,24 @@ class TeamStatsTest < Minitest::Test
   end
 
   def test_teams_best_and_worst_season_based_on_win_percentage_can_be_shown
-    #expected = Integer
+    #original expected = Integer
     #spec harness now says needs to convert to string "20122013"
-    assert_equal 2012, @stat_tracker.best_season(17) #50%
-    assert_equal 2011, @stat_tracker.worst_season(17) #0%
+    assert_equal 2012, @stat_tracker.best_season(17) # => "20122013"
+    assert_equal 2011, @stat_tracker.worst_season(17) # => "20112012"
   end
 
   def test_teams_average_win_percentage_of_all_games_is_shown
-    assert_equal 25.0, @stat_tracker.average_win_percentage(17)
+    assert_equal 0.25, @stat_tracker.average_win_percentage(17)
   end
 
   def test_teams_highest_and_lowest_number_of_goals_in_a_single_game_is_shown
-    # expected = Integ
     assert_equal 4, @stat_tracker.most_goals_scored(17)
     assert_equal 0, @stat_tracker.fewest_goals_scored(17)
   end
 
-  # def test_teams_favorite_opponents_name_is_shown
-  #   expected = String
-  #   assert_equal "Blackhawks", @stat_tracker.favorite_opponent(17)
-  # end
-
-  # def test_teams_rivals_name_is_shown
-  #   expected = String
-  #   assert_equal "Blackhawks", @stat_tracker.rival(17)
-  # end
-
   def test_all_wins_and_losses_for_a_team_can_be_shown
-    # array of 3 game_team objects for each (team happens to be 3-3)
-    assert_equal 2, @stat_tracker.all_wins_by_team(17).count #2
-    assert_equal 4, @stat_tracker.all_losses_by_team(17).count #4
+    assert_equal 2, @stat_tracker.all_wins_by_team(17).count
+    assert_equal 4, @stat_tracker.all_losses_by_team(17).count
   end
 
   def test_teams_biggest_blowout_win__and_worst_loss_by_goal_differential_is_shown
@@ -93,8 +77,8 @@ class TeamStatsTest < Minitest::Test
 
     @st = StatTracker.from_csv(@locations)
 
-    assert_equal 3, @st.biggest_team_blowout(17) #using game_team_stats_fixture.csv
-    assert_equal 5, @st.worst_loss(17) #using game_team_stats_fixture.csv
+    assert_equal 3, @st.biggest_team_blowout(17)
+    assert_equal 5, @st.worst_loss(17)
   end
 
   def test_teams_head_to_head_record_is_shown_versus_a_specific_opponent
@@ -124,10 +108,22 @@ class TeamStatsTest < Minitest::Test
     expected_2 = ({"Predators"=>3, "Maple Leafs"=>2})
 
     assert_equal expected_1, @st.all_wins_vs_opponent(17)
-    # binding.pry
     assert_equal expected_2, @st.all_games_vs_opponent(17)
   end
 
+  def test_rival_and_favorite_opponent_can_be_shown
+    @game_path = './data/game_fixture_opponent_testing.csv'
+    @team_path = './data/team_info.csv'
+    @game_teams_path = './data/game_team_stats_fixture_opponent_testing.csv'
+    @locations = {games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path}
+
+    @st = StatTracker.from_csv(@locations)
+
+    assert_equal "Predators", @st.favorite_opponent(17)
+    assert_equal "Maple Leafs", @st.rival(17)
+  end
 
   # def test_teams_seasonal_summary_is_shown_for_each_season_it_has_played
   #   # each season, a hash with two keys (:preseason, :regular_season)
