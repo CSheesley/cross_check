f454768245b3d2ce32119c86f10d05d9cb8782e0
Chi Tran <chidtran@gmail.com>
2019-02-19 12:24:47 -0700

    Moves helper methods from GameStats to List module

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -9,20 +9,8 @@ module GameStats
     total_scores.min
   end
 
-  def total_scores
-    @games.repo.map do |game|
-      game.away_goals + game.home_goals
-    end
-  end
-
   def biggest_blowout
     teams_score_difference.max.abs
-  end
-
-  def teams_score_difference
-    @games.repo.map do |game|
-      (game.away_goals - game.home_goals)
-    end
   end
 
   def percentage_home_wins
@@ -31,18 +19,6 @@ module GameStats
 
   def percentage_visitor_wins
     (total_visitor_wins/total_games.to_f).round(2)
-  end
-
-  def total_games
-    @games.repo.map do |game|
-      game.game_id
-    end.size
-  end
-
-  def total_visitor_wins
-    teams_score_difference.select do |score_diff|
-      score_diff > 0
-    end.size
   end
 
   def count_of_games_by_season
@@ -55,24 +31,8 @@ module GameStats
     games_by_season
   end
 
-  # def games_by_season
-  #   @games.repo.group_by do |game|
-  #     game.season
-  #   end
-  # end
-
   def average_goals_per_game
     (total_goals_all_seasons/total_games.to_f).round(2)
-  end
-
-  def total_goals_all_seasons
-    total_away_goals = @games.repo.sum do |game|
-      game.away_goals
-    end
-    total_home_goals = @games.repo.sum do |game|
-      game.home_goals
-    end
-    total_away_goals + total_home_goals
   end
 
   def average_goals_by_season
