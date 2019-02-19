b5afbc9b061cfbc9f5381b4f6c20f241f9ed8918
Chi Tran <chidtran@gmail.com>
2019-02-19 16:31:45 -0700

    Refactors GameStats

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -22,14 +22,14 @@ module GameStats
   end
 
   def count_of_games_by_season
-    games_by_season = @games.repo.group_by do |game|
-      game.season
-    end
-    games_by_season.each do |season, games|
-      games_by_season[season] = games.count
-    end
-    games_by_season
+    count_of_games_by_season = {}
+    games_by_season.each { |season, games| count_of_games_by_season[season] = games.count }
+    count_of_games_by_season
   end
+
+  def games_by_season #HELPER method, Carrie may have this helper method in stattracker already 
+    @games.repo.group_by { |game| game.season }
+  end 
 
   def average_goals_per_game
     (total_goals_all_seasons/total_games.to_f).round(2)
@@ -37,9 +37,6 @@ module GameStats
 
   def average_goals_by_season
     average_goals_by_season = {}
-    games_by_season = @games.repo.group_by do |game|
-      game.season
-    end
     games_by_season.each do |season, games|
       goals = games.sum do |game|
         (game.away_goals + game.home_goals)
