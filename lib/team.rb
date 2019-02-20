1fa8e679b5e19d5fe473e380ac47846c367c8900
Carrie Walsh <carriewalsh@gmail.com>
2019-02-19 19:56:47 -0700

    Aligns team init to csv file

--- lib/team.rb
+++ lib/team.rb
@@ -8,9 +8,9 @@ class Team
 
   def initialize(team_info)
     @team_id      = team_info[:team_id]
-    @franchise_id = team_info[:franchise_id]
-    @short_name   = team_info[:short_name]
-    @team_name    = team_info[:team_name]
+    @franchise_id = team_info[:franchiseid]
+    @short_name   = team_info[:shortname]
+    @team_name    = team_info[:teamname]
     @abbreviation = team_info[:abbreviation]
     @link         = team_info[:link]
   end
