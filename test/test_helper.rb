b9a9c5410e4b0940457319abf19275cfed9f9b33
Chi Tran <chidtran@gmail.com>
2019-02-20 14:06:54 -0700

    Changes to single quotes in TestHelper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,21 @@
-require "simplecov"
+require 'simplecov'
 SimpleCov.start
 
-require "minitest/autorun"
-require "minitest/pride"
-require "pry"
-require "csv"
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require 'csv'
 
-require "./lib/stat_tracker"
-require "./lib/game_team"
-require "./lib/game"
-require "./lib/team"
+require './lib/stat_tracker'
+require './lib/game_team'
+require './lib/game'
+require './lib/team'
 
-require "./lib/game_stats"
-require "./lib/season_stats"
-require "./lib/team_stats"
-require "./lib/league_stats"
+require './lib/game_stats'
+require './lib/season_stats'
+require './lib/team_stats'
+require './lib/league_stats'
 
-require "./lib/game_repo"
-require "./lib/game_team_repo"
-require "./lib/team_repo"
+require './lib/game_repo'
+require './lib/game_team_repo'
+require './lib/team_repo'
